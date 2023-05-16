219ec8582fa61be169a6830ed87ccfdd8277650b
naukite <naukite@gmail.com>
2023-05-16 19:00:39 +0000

    feat: se añaden pruebas para comprobar las api

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -21,3 +21,43 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
+
+    #se añade prueba para comprobar la resta
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )        
+
+    #se añade prueba para comprobar la división
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )                
+
+    #se añade prueba para comprobar la potencia
+    def test_api_power(self):
+        url = f"{BASE_URL}/calc/power/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )                        
+
+    #se añade prueba para comprobar la raíz cuadrada
+    def test_api_sqrt(self):
+        url = f"{BASE_URL}/calc/sqrt/4"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )                                
+
+    #se añade prueba para comprobar el log10
+    def test_api_log10(self):
+        url = f"{BASE_URL}/calc/log10/10"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )                                        
